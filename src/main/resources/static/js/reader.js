908a3d9d39dd9208d95e75f9de9517341409fc1f
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2022-06-14 17:47:11 -0300

    Botões de zoom apenas para PC

--- src/main/resources/static/js/reader.js
+++ src/main/resources/static/js/reader.js
@@ -10,3 +10,24 @@ for(let i = 0; i < pointers.length; i ++){
         document.location.href = "/reader?url=" + pointers[i].getAttribute("point-to");
     });
 }
+
+function checkDevice() {
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        return true; // está utilizando celular
+    }
+    else {
+        return false; // não é celular
+    }
+}
+
+if(checkDevice()) {
+    document.querySelector("#zoomout").classList.add("d-none");
+    document.querySelector("#zoomin").classList.add("d-none");
+}
